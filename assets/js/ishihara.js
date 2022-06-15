2dc2a46c3bfa0caa73e44ca1d1ec44b795591be9
Rodrigo Montero <rodrigomontero89@gmail.com>
2022-06-15 19:59:49 -0300

    Eficiencia de for

--- assets/js/ishihara.js
+++ assets/js/ishihara.js
@@ -1,11 +1,10 @@
-
 /**
 * Name: Daltest, test de ishihara
 * Author: Rodrigo Montero
 */
 
 let resultado = 0;
-let respuestas = '';
+let respuestas = [];
 const respuestasCorrectas = ['74', '6', '16', '2', '29', '7', '45', '5', '97', '8', '42', '3'];
 
 for (let i = 1; i <= 12; i++) {
@@ -13,122 +12,24 @@ for (let i = 1; i <= 12; i++) {
     if (numeroIngresado == 0) {
         i = 13;
     } else {
-        switch (i) {
-            case 1:
-                respuestas = numeroIngresado 
-                if (numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(0,1) || numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(0,1)) {
-                    console.log(numeroIngresado.substring(0,1) + ' ' + respuestasCorrectas[i-1].substring(0,1))
-                    resultado = resultado + 1
-                } 
-                if (numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(1,2) || numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(1,2)) {
-                    console.log(numeroIngresado.substring(1,2) + ' ' + respuestasCorrectas[i-1].substring(1,2))
-                    resultado = resultado + 1
-                }
-                break;
-            case 2:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado == respuestasCorrectas[i-1]) {
-                    console.log(numeroIngresado + ' ' + respuestasCorrectas[i-1])
-                    resultado = resultado + 1
-                }
-                break;
-            case 3:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(0,1) || numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(0,1)) {
-                    console.log(numeroIngresado.substring(0,1) + ' ' + respuestasCorrectas[i-1].substring(0,1))
-                    resultado = resultado + 1
-                }
-                if (numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(1,2) || numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(1,2)) {
-                    console.log(numeroIngresado.substring(1,2) + ' ' + respuestasCorrectas[i-1].substring(1,2))
-                    resultado = resultado + 1
-                }
-                break;
-            case 4:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado == respuestasCorrectas[i-1]) {
-                    console.log(numeroIngresado + ' ' + respuestasCorrectas[i-1])
-                    resultado = resultado + 1
-                }
-                break;
-            case 5:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(0,1) || numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(0,1)) {
-                    console.log(numeroIngresado.substring(0,1) + ' ' + respuestasCorrectas[i-1].substring(0,1))
-                    resultado = resultado + 1
-                } 
-                if (numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(1,2) || numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(1,2)) {
-                    console.log(numeroIngresado.substring(1,2) + ' ' + respuestasCorrectas[i-1].substring(1,2))
-                    resultado = resultado + 1
-                }
-                break;
-            case 6:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado == respuestasCorrectas[i-1]) {
-                    console.log(numeroIngresado + ' ' + respuestasCorrectas[i-1])
-                    resultado = resultado + 1
-                }
-                break;
-            case 7:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(0,1) || numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(0,1)) {
-                    console.log(numeroIngresado.substring(0,1) + ' ' + respuestasCorrectas[i-1].substring(0,1))
-                    resultado = resultado + 1
-                } 
-                if (numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(1,2) || numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(1,2)) {
-                    console.log(numeroIngresado.substring(1,2) + ' ' + respuestasCorrectas[i-1].substring(1,2))
-                    resultado = resultado + 1
-                }
-                break;
-            case 8:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado == respuestasCorrectas[i-1]) {
-                    console.log(numeroIngresado + ' ' + respuestasCorrectas[i-1])
-                    resultado = resultado + 1
-                }
-                break;
-            case 9:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(0,1) || numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(0,1)) {
-                    console.log(numeroIngresado.substring(0,1) + ' ' + respuestasCorrectas[i-1].substring(0,1))
-                    resultado = resultado + 1
-                } 
-                if (numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(1,2) || numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(1,2)) {
-                    console.log(numeroIngresado.substring(1,2) + ' ' + respuestasCorrectas[i-1].substring(1,2))
-                    resultado = resultado + 1
-                }
-                break;
-            case 10:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado == respuestasCorrectas[i-1]) {
-                    console.log(numeroIngresado + ' ' + respuestasCorrectas[i-1])
-                    resultado = resultado + 1
-                }
-                break;
-            case 11:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(0,1) || numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(0,1)) {
-                    console.log(numeroIngresado.substring(0,1) + ' ' + respuestasCorrectas[i-1].substring(0,1))
-                    resultado = resultado + 1
-                }
-                if (numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(1,2) || numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(1,2)) {
-                    console.log(numeroIngresado.substring(1,2) + ' ' + respuestasCorrectas[i-1].substring(1,2))
-                    resultado = resultado + 1
-                }
-                break;
-            case 12:
-                respuestas = respuestas + ', ' + numeroIngresado 
-                if (numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(0,1) || numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(0,1)) {
-                    console.log(numeroIngresado + ' ' + respuestasCorrectas[i-1])
-                    resultado = resultado + 1
-                }
-                if (resultado != 0) {
-                    alert('Respuestas: ' + respuestas)
-                    alert('Respuestas correctas: ' + respuestasCorrectas.join())
-                    alert('Resultado: ' + ((resultado*100)/18) + '% (' + resultado + '/18)')
-                }
-                break;
+        respuestas.push(numeroIngresado);
+        if (respuestasCorrectas[i-1].substring(0,1)) {
+            if (numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(0,1) || numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(0,1)) {
+                resultado = resultado + 1;
+            }
+        }
+        if (respuestasCorrectas[i-1].substring(1,2)) {
+            if (numeroIngresado.substring(1,2) == respuestasCorrectas[i-1].substring(1,2) || numeroIngresado.substring(0,1) == respuestasCorrectas[i-1].substring(1,2)) {
+                resultado = resultado + 1;
             }
         }
     }
+}
+
+if (resultado != 0) {
+    alert('Respuestas: ' + respuestas.join());
+    alert('Respuestas correctas: ' + respuestasCorrectas.join());
+    alert('Resultado: ' + ((resultado*100)/18) + '% (' + resultado + '/18)');
+}
 
 
--- assets/js/ninios.js
+++ assets/js/ninios.js
@@ -1,11 +1,10 @@
-
 /**
 * Name: Daltest, test para niños
 * Author: Rodrigo Montero
 */
 
 let resultado = 0;
-let respuestas = '';
+let respuestas = [];
 const respuestasCorrectas = ['H','E','B','J','D','G','A','F','C','I'];
 
 for (let i = 1; i <= 10; i++) {
@@ -13,74 +12,17 @@ for (let i = 1; i <= 10; i++) {
     if (letraIngresada == 'ESC' || letraIngresada == '') {
         i = 13;
     } else {
-        switch (i) {
-            case 1:
-                respuestas = letraIngresada;
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                break;
-            case 2:
-                respuestas = respuestas + ', ' + letraIngresada;
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                break;
-            case 3:
-                respuestas = respuestas + ', ' + letraIngresada
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                break;
-            case 4:
-                respuestas = respuestas + ', ' + letraIngresada 
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                break;
-            case 5:
-                respuestas = respuestas + ', ' + letraIngresada 
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                break;
-            case 6:
-                respuestas = respuestas + ', ' + letraIngresada 
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                break;
-            case 7:
-                respuestas = respuestas + ', ' + letraIngresada 
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                break;
-            case 8:
-                respuestas = respuestas + ', ' + letraIngresada
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                break;
-            case 9:
-                respuestas = respuestas + ', ' + letraIngresada
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                break;
-            case 10:
-                respuestas = respuestas + ', ' + letraIngresada 
-                if (letraIngresada == respuestasCorrectas[i-1]) {
-                    resultado = resultado + 1
-                }
-                if (resultado != 0) {
-                    alert('Respuestas: ' + respuestas)
-                    alert('Respuestas correctas: ' + respuestasCorrectas.join())
-                    alert('Resultado: ' + ((resultado*100)/10) + '% (' + resultado + '/10)')
-                }
-                break;
-            }
+        respuestas.push(letraIngresada);
+        if (letraIngresada == respuestasCorrectas[i-1]) {
+            resultado = resultado + 1;
         }
     }
+}
+
+if (resultado != 0) {
+    alert('Respuestas: ' + respuestas.join());
+    alert('Respuestas correctas: ' + respuestasCorrectas.join());
+    alert('Resultado: ' + ((resultado*100)/10) + '% (' + resultado + '/10)');
+}
 
 
